7c66a4d5c6794b561beb6750bf64640c0c3e41db
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-04-02 09:39:22 -0500

    Criando Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import { Flex, Text, Input, Icon } from '@chakra-ui/react';
+import { RiSearchLine } from 'react-icons/ri';
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1080}
+      h="20"
+      mx="4"
+      px="6"
+      align="center"
+    >
+      <Text fontSize="3xl" fontWeight="bold" latterSpacing="tight" w="64">
+        DashGo
+        <Text as="span" ml="1" color="pink.500">
+          .
+        </Text>
+      </Text>
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          px="4"
+          mr="4"
+          placeholder="Buscar na Plataforma"
+          _placeholder={{ color: 'gray.400' }}
+        />
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+    </Flex>
+  );
+}
